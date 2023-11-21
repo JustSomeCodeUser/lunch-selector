8fae7b0e209bd5c9f110c0ec1cc5c88f99729642
singkeat.wong <singkeat.wong@wirecard.com>
2023-11-21 20:14:22 +0800

    set up page routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {RestaurantInputComponent} from "./restaurant-input/restaurant-input.component";
+import {RandomRestaurantComponent} from "./random-restaurant/random-restaurant.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, RestaurantInputComponent, RandomRestaurantComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
